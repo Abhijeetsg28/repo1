dd33628dee42013fb40d3b7569c74cdd21c0bf0e
abhijeet.garudkar <abhijeet.garudkar@N12916.intellectdesign.com>
2021-09-16 11:48:47 +0530

    FirstRepository

---
+++ myapp1_myapp1/src/main/java/com/myapp/Myapp1Application.java
@@ -0,0 +1,14 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myapp1Application.class, args);
+		
+		System.out.println("Welcome to Booot Abhijeet");	}
+
+}
